04e9c740fbdb613af0828cf4997ae503f70b177b
KhAbdukhoshimov <Khangitlab2710>
2023-02-17 11:51:46 +0500

    Updated report-job life time

--- src/DailyLeetcodeReminder.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/DailyLeetcodeReminder.Core/Extensions/ServiceCollectionExtensions.cs
@@ -137,7 +137,7 @@ public static class ServiceCollectionExtensions
             q.AddTrigger(opts => opts
                 .ForJob(dailyReportJobKey)
                 .WithIdentity($"{dailyReportJobKey.Name}-trigger")
-                .WithCronSchedule("0 26 11 * * ?")
+                .WithCronSchedule("0 54 11 * * ?")
             );
 
             var dailyProblemJobKey = new JobKey(nameof(DailyProblemJob));
