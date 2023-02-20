95f9ec4aa97026ca4ffec7f6e82ef67e1766eb12
Zayniddinov Zuhriddin <zayniddinovzuhriddin99@gmail.com>
2023-02-20 15:20:24 +0500

    CODE RUB: S LOVER CASE CONVERT

--- src/DailyLeetcodeReminder.Infrastructure/Jobs/DailyReportJob.cs
+++ src/DailyLeetcodeReminder.Infrastructure/Jobs/DailyReportJob.cs
@@ -132,14 +132,14 @@ public class DailyReportJob : IJob
 
         foreach (var challenger in activeChallengers)
         {
-            int? SolvedProblems = challenger.DailyAttempts.Where(da => da.Date != DateTime.Now.Date)
+            int? solvedProblems = challenger.DailyAttempts.Where(da => da.Date != DateTime.Now.Date)
                             .FirstOrDefault().SolvedProblems;
-            if (SolvedProblems == null)
+            if (solvedProblems == null)
                 continue;
             messageBuilder.AppendLine(String.Format("| {0, -20} | {1, -6}| {2, -6}| {3, -6}|",
                         challenger.LeetcodeUserName,
                         challenger.Heart,
-                        SolvedProblems,
+                        solvedProblems,
                         challenger.TotalSolvedProblems)); 
         }
 
