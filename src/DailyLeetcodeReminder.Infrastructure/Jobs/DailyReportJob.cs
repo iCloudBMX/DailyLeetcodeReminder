b905f6eccdbdf90d8438ffd520a8d4695d5b4a2e
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-03 01:30:48 +0500

    fixed daily report

--- src/DailyLeetcodeReminder.Infrastructure/Jobs/DailyReportJob.cs
+++ src/DailyLeetcodeReminder.Infrastructure/Jobs/DailyReportJob.cs
@@ -121,19 +121,24 @@ public class DailyReportJob : IJob
 
     private string GetDailyReportDetails(List<Challenger> activeChallengers)
     {
+        int maximalUsername = 0;
+        foreach (var username in activeChallengers)
+        {
+            maximalUsername = Math.Max(username.LeetcodeUserName.Length, maximalUsername);
+        }
         StringBuilder messageBuilder = new();
 
-        messageBuilder.AppendLine($"Hisobot - <b>{DateTime.Now.ToString("dd.MM.yyyy")}</b>\n");
+        messageBuilder.AppendLine($"Hisobot - <b>{DateTime.Now.ToString("dd.MM.yyyy")}</b>");
 
-        messageBuilder.AppendLine($"<pre>|{new string('-', 22)}" +
+        messageBuilder.AppendLine($"<pre>|{new string('-', (maximalUsername + 2))}" +
                                        $"|{new string('-', 7)}" +
                                        $"|{new string('-', 7)}" +
                                        $"|{new string('-', 7)}|");
 
-        messageBuilder.AppendLine(String.Format("| {0, -20} | {1, -6}| {2, -6}| {3, -6}|",
+        messageBuilder.AppendLine(String.Format("| {0, -"+ maximalUsername +"} | {1, -6}| {2, -6}| {3, -6}|",
                                                 "Foydalanuvchi nomi", "Yurak", "Bugun", "Jami"));
 
-        messageBuilder.AppendLine($"|{new string('-', 22)}" +
+        messageBuilder.AppendLine($"|{new string('-', (maximalUsername + 2))}" +
                                   $"|{new string('-', 7)}" +
                                   $"|{new string('-', 7)}" +
                                   $"|{new string('-', 7)}|");
@@ -152,7 +157,7 @@ public class DailyReportJob : IJob
                 continue;
             }
 
-            messageBuilder.AppendLine(String.Format("| {0, -20} | {1, -6}| {2, -6}| {3, -6}|",
+            messageBuilder.AppendLine(String.Format("| {0, -"+ maximalUsername +"} | {1, -6}| {2, -6}| {3, -6}|",
                         challenger.LeetcodeUserName,
                         challenger.Heart,
                         yesterDayAttempt.SolvedProblems,
